abd970c1363ce287c346e36aee59649d80dea8fb
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-07-21 01:12:40 -0400

    plantUML updated to rough draft

--- src/main/java/ucf/assignments/FileFunctions.java
+++ src/main/java/ucf/assignments/FileFunctions.java
@@ -1,4 +1,18 @@
 package ucf.assignments;
 
 public class FileFunctions {
+    //store as JSON
+    public void storeAsJSON(){
+
+    }
+
+    //store as HTML
+    public void storeAsHTML(){
+
+    }
+
+    //store as TSV
+    public void storeAsTSV(){
+
+    }
 }
--- src/main/java/ucf/assignments/InventoryFunctions.java
+++ src/main/java/ucf/assignments/InventoryFunctions.java
@@ -1,4 +1,33 @@
 package ucf.assignments;
 
+import javafx.collections.ObservableList;
+
 public class InventoryFunctions {
+    public void addItem(String value, String serialNumber, String name){
+
+    }
+
+    public void deleteItem(ItemWrapper selectedItem, ObservableList<ItemWrapper> allItemsList){
+
+    }
+
+    public void editName(){
+
+    }
+
+    public void editValue(){
+
+    }
+
+    public void editSerialNumber(){
+
+    }
+
+    public void searchByName(){
+
+    }
+
+    public void searchByValue(){
+
+    }
 }
