cdc1f510e991a226c4b16d59c6c8e52727039bcb
ivy-nguyen18 <nguyen.ivy1802@knights.ucf.edu>
2021-07-23 00:10:38 -0400

    file chooser created

--- src/main/java/ucf/assignments/FileFunctions.java
+++ src/main/java/ucf/assignments/FileFunctions.java
@@ -1,5 +1,10 @@
 package ucf.assignments;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class FileFunctions {
     //store as JSON
     public void storeAsJSON(){
@@ -15,4 +20,29 @@ public class FileFunctions {
     public void storeAsTSV(){
 
     }
+
+    public File openFile(){
+        //create FileChooser
+        FileChooser fileChooser = new FileChooser();
+
+        //create stage for fileChooser
+        Stage fileStage = new Stage();
+        fileStage.setTitle("Open");
+
+        //set default directory to be user's home directory
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        //make sure file is saved as .ser to be serializable
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JSON file","*.json"),
+                new FileChooser.ExtensionFilter("HTML file", "*.html"),
+                new FileChooser.ExtensionFilter("TSV file","*.txt"));
+
+        //show the file stage
+
+        //change instance of selected file to selected file in FileChooser
+        return fileChooser.showOpenDialog(fileStage);
+
+    }
+
+
 }
